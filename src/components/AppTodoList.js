9e938292a678311b459b71cf0e5fc6173167a4ff
LOUTFI Khalil <loutfi.khalil@gmail.com>
2020-07-07 02:08:34 +0100

    all-is-ok

--- src/components/AppTodoList.js
+++ src/components/AppTodoList.js
@@ -17,6 +17,22 @@ class AppTodoList extends Component {
     };
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !==""){
+      const items = [...this.state.items, newItem];
+    this.setState({
+      items: items,
+      currentItem:{
+        text:'',
+        key:''
+      }
+    })
+    }
   }
   handleInput(e) {
     this.setState({
@@ -41,9 +57,33 @@ class AppTodoList extends Component {
       });
     }
   }
+  deleteItem(key){
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+  setUpdate(text,key){
+    console.log("items:"+this.state.items);
+    const items = this.state.items;
+    items.map(item=>{      
+      if(item.key===key){
+        console.log(item.key +"    "+key)
+        item.text= text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+    
+   
+  }
   render() {
     return (
       <div className="TodoList">
+        <h1>Simple Todo List Whith React and Css</h1>
         <form id="to-do-form" onSubmit={this.addItem}>
           <input
             type="text"
@@ -52,7 +92,7 @@ class AppTodoList extends Component {
             onChange={this.handleInput}
           />
           <button type="submit">Add New </button>
-          <ListTodoInApp items={this.state.items} />
+          <ListTodoInApp items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate} />
         </form>
       </div>
     );
--- src/components/ListTodoApp.js
+++ src/components/ListTodoApp.js
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
 import "./ListTodoCss.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import FlipMove from 'react-flip-move';
 
 function ListTodoInApp(props) {
-  const items = props.items;
-  const listItems = items.map((item) => {
-    return (
-      <div className="list" key={item.key}>
-        <p>{item.text}</p>
+   const items = props.items;
+    const listItems = items.map(item =>
+   {
+       return <div className="list" key={item.key}>
+       <p> 
+         <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+         
+            props.setUpdate(e.target.value,item.key)}}/>
         <span>
-           <FontAwesomeIcon className="faicons" onClick={() => {
-           props.deleteItem(item.key)
-       }} icon="trash" />
-       </span>
-      </div>
-    );
-  });
-  return <div>{listItems}</div>;
-}
+       
+        <FontAwesomeIcon className="faicons" onClick={() => {
+            props.deleteItem(item.key)
+        }} icon="trash" />
+        </span>
+     </p>
+     
+    </div>})
+    return <div>
+        <FlipMove duration={700} easing="ease-in-out">
+        {listItems}
+        </FlipMove>
+    
+    </div>;
+  }
 
 export default ListTodoInApp;
